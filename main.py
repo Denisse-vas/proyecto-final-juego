19d1fb7b677397982f3f6d2ca96710f6cc5accd4
CarlosVeneRu <Carlos_Venegas_01@Outlook.com>
2025-06-11 18:02:12 -0600

    Prototipo completo V.1.0

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from random import randrange
+
+
 # Mostrar Tablero
 def display_board(board):
 	print("+-------" * 3,"+", sep="")
@@ -36,11 +39,59 @@ def make_list_of_free_fields(board):
 			if board[row][col] not in ['O','X']: # ¿Está la celda libre?
 				free.append((row,col)) # si, agrega una nueva tupla a la lista
 	return free
+
+# Condición de Victoria y Empate contra máquina
+def victory_for(board,sgn):
+	if sgn == "X":	# ¿Estamos buscando X?
+		who = 'me'	# Si, es la maquina
+	elif sgn == "O": # ... ¿o estamos buscando O?
+		who = 'you'	# Si, es el usuario
+	else:
+		who = None	# ¡No debemos de caer aquí!
+	cross1 = cross2 = True  # para las diagonales
+	for rc in range(3):
+		if board[rc][0] == sgn and board[rc][1] == sgn and board[rc][2] == sgn:	# check row rc
+			return who
+		if board[0][rc] == sgn and board[1][rc] == sgn and board[2][rc] == sgn: # check column rc
+			return who
+		if board[rc][rc] != sgn: # revisar la primer diagonal
+			cross1 = False
+		if board[2 - rc][2 - rc] != sgn: # revisar la segunda diagonal
+			cross2 = False
+	if cross1 or cross2:
+		return who
+	return None
+
+def draw_move(board):
+	free = make_list_of_free_fields(board) # crea una lista de los cuadros vacios o libres
+	cnt = len(free)
+	if cnt > 0:	# si la lista no esta vacía, elegir un lugar para 'X' y colocarla
+		this = randrange(cnt)
+		row, col = free[this]
+		board[row][col] = 'X'
 		
 
 board = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ] # crear un tablero vacío
 board[1][1] = 'X' # colocar la primer 'X' en el centro
 free = make_list_of_free_fields(board)
 human_turn = True # ¿De quien es turno ahora?
+while len(free):
+	display_board(board)
+	if human_turn:
+		enter_move(board)
+		victor = victory_for(board,'O')
+	else:
+		draw_move(board)
+		victor = victory_for(board,'X')
+	if victor != None:
+		break
+	human_turn = not human_turn
+	free = make_list_of_free_fields(board)
 
 display_board(board)
+if victor == 'you':
+	print("¡Has ganado!")
+elif victor == 'me':
+	print("¡He ganado!")
+else:
+	print("¡Empate!")
